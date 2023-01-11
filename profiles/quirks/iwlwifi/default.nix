38f4b151a06c0c013196c32e27c1ac4724580791
Noah Fontes <noah@noahfontes.com>
2023-01-11 10:49:57 -0800

    Only build custom kernel when needed

--- profiles/quirks/iwlwifi/default.nix
+++ profiles/quirks/iwlwifi/default.nix
@@ -1,10 +1,10 @@
-# SPDX-FileCopyrightText: 2022 Noah Fontes
+# SPDX-FileCopyrightText: 2022-2023 Noah Fontes
 #
 # SPDX-License-Identifier: CC-BY-NC-SA-4.0
 
-{
+{ config, lib, ... }: with lib; {
   # https://bugzilla.kernel.org/show_bug.cgi?id=203709
-  boot.kernelPatches = [
+  boot.kernelPatches = mkIf config.profiles.wireless.enable [
     {
       patch = ./iwlwifi-missed-beacons-timeout.patch;
     }
