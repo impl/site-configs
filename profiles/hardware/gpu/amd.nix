fd4683b89775917c97834b957284c369a9699bf2
Noah Fontes <noah@noahfontes.com>
2026-01-16 09:28:14 -0800

    Try to work around amdgpu hangs

--- profiles/hardware/gpu/amd.nix
+++ profiles/hardware/gpu/amd.nix
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2021-2025 Noah Fontes
+# SPDX-FileCopyrightText: 2021-2026 Noah Fontes
 #
 # SPDX-License-Identifier: CC-BY-NC-SA-4.0
 
@@ -34,6 +34,9 @@ in
       };
     }
     (mkIf config.profiles.gui.enable {
+      # Disable panel self-refresh (PSR) because it keeps making the whole system freeze.
+      boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
+
       services.xserver = mkIf (!config.profiles.hardware.gpu.nvidia.enable) {
         videoDrivers = [ "amdgpu" ];
         deviceSection = ''
